66263eed321f997f0e34160de5b0a2954b53ffb5
Paul Gallo <paul.gallo@octo.com>
2022-03-03 10:25:48 +0100

    add config for api token generation

--- config/admin.js
+++ config/admin.js
@@ -1,4 +1,7 @@
 module.exports = ({ env }) => ({
+  apiToken: {
+    salt: env('API_TOKEN_SALT', '23981f0oede39p5984gcd99mf007aw67y'),
+  },
   auth: {
     secret: env('ADMIN_JWT_SECRET', '13101f0ece39b5194bdd79bf597fc67b'),
   },
